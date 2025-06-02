e9c39da6102987578ba57de6deefb91ab91f8929
Do it special <74970585+IntellegentOxid@users.noreply.github.com>
2025-06-02 04:22:03 +0200

    1

--- 29. Прочитати масив чисел, при помилці — записати 0/main.kt
+++
@@ -1,9 +0,0 @@
-val inputs = listOf("10", "abc", "25", "7.5", "-3")
-val numbers = inputs.map {
-    try {
-        it.toInt()
-    } catch (e: NumberFormatException) {
-        0
-    }
-}
-println("Parsed numbers: $numbers")
